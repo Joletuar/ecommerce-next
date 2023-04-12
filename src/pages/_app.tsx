ec2ce31d791d895e5f22b7b4e4c27c5e9ce6c14d
Johan <96794364+Joletuar@users.noreply.github.com>
2023-04-12 16:03:09 +0000

    Limpieza del proyecto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { lightTheme } from '@/themes';
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
